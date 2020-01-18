5b02b95eedee31b4a089efc4252f918b48843de2
TOGURO Mikito <mit@shalab.net>
2020-01-18 17:00:36 +0900

    To improve robustness, a message is now output if imgui_imm32_onthespot.h is not loaded before imgui_imm32_onthespot_sdl.h.

--- IMM32IMGUI/imgui_imm32_onthespot_sdl.h
+++ IMM32IMGUI/imgui_imm32_onthespot_sdl.h
@@ -3,7 +3,15 @@
 #if !defined(IMGUI_IMM32_ONTHESPOT_SDL_H_HEADER_GUARD_7058bc82_a46f_4c73_9af3_b08c91eacfac)
 #define IMGUI_IMM32_ONTHESPOT_SDL_H_HEADER_GUARD_7058bc82_a46f_4c73_9af3_b08c91eacfac 1
 
+#if !defined(IMGUI_IMM32_ONTHESPOT_H_UUID_ccfbd514_0a94_4888_a8b8_f065c57c1e70_HEADER_GUARD)
+#include "imgui_imm32_onthespot.h"
+#if defined(_MSC_VER)
+#pragma message("require include imgui_imm32_onthespot.h before imgui_imm32_onthespot_sdl.h")
+#endif /* defined( _MCS_VER ) */
+#endif /*!defined(IMGUI_IMM32_ONTHESPOT_H_UUID_ccfbd514_0a94_4888_a8b8_f065c57c1e70_HEADER_GUARD) */
+
 #include <SDL.h>
+#include <SDL_syswm.h>
 
 #if defined( __cplusplus )
 
@@ -15,7 +23,7 @@ ImGUIIMMCommunication::subclassify<SDL_Window*>(SDL_Window* window )
   SDL_SysWMinfo info{};
   SDL_VERSION(&info.version);
   if (SDL_GetWindowWMInfo(window, &info)) {
-    assert(IsWindow(info.info.win.window));
+    IM_ASSERT(IsWindow(info.info.win.window));
     return this->subclassify( info.info.win.window );
   }
   return FALSE;
